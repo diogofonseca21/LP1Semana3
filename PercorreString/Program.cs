117354a86947abf0df2db40f4975202f8ab77484
diogofonseca21 <diogosfonseca.live@gmail.com>
2022-03-17 14:16:48 +0000

    first commit

---
+++ PercorreString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
