32f067c1f9be3b0dc223c8514ee146eb70cf433c
diogofonseca21 <diogosfonseca.live@gmail.com>
2022-03-17 14:49:13 +0000

    exercicio 4 sem2

--- PercorreString/Program.cs
+++ PercorreString/Program.cs
@@ -6,7 +6,19 @@ namespace PercorreString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float pi = 3.14f;
+
+            Console.WriteLine("Introduza a altura");
+            float altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduza o raio");
+            float raio = float.Parse(Console.ReadLine());
+
+            float volume = pi * (raio*raio) * altura;
+            float area = 2 * pi * raio* (raio + altura);
+
+            Console.WriteLine(volume);
+            Console.WriteLine(area);
         }
     }
 }
